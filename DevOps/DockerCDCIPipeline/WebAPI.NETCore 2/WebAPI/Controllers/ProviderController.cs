72d3375a832ed5b259f312238bf39ee13fefeb43
Nilay Parikh <nilay.parikh@ergosum.in>
2018-03-02 17:26:32 +0000

    initial checkin

---
+++ DevOps/DockerCDCIPipeline/WebAPI.NETCore 2/WebAPI/Controllers/ProviderController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProviderController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id = 11)
+        {
+            return "value " + id;
+        }
+    }
+}
